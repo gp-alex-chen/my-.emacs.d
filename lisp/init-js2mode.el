67ed7a64b39bb36be03bfa896563b5a99a5dab10
alex-chen <yuanzhang93.chen@gmail.com>
2016-05-02 10:41:23 +0800

    add some plugin

---
+++ lisp/init-js2mode.el
@@ -0,0 +1,5 @@
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+(provide 'init-js2mode)
---
+++ lisp/init-swiper.el
@@ -0,0 +1,7 @@
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+(provide 'init-swiper)
