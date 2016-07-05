c81911bd9123bb251f6a40720361b35e03b5c12f
Alex-Chen <yuanzhang93.chen@gmail.com>
2016-07-05 19:49:11 +0800

    fix some bugs

--- init.el
+++ init.el
@@ -19,15 +19,6 @@
 
 ;; js 相关
 (require 'init-js2mode)
-(require 'nodejs-repl)
-;; mode 使用
-(setq auto-mode-alist
-      (append
-       '(("\\.html\\'" . web-mode)
-	 ("\\.css\\'" . web-mode)
-	 )
-       auto-mode-alist))
-(exec-path-from-shell-initialize)
 
 ;; popwin
 (require 'popwin)
@@ -62,17 +53,4 @@
 (provide 'init)
 
 ;;;;;;;;;;;;;;;;;;;;   end   ;;;;;;;;;;;;;;;;;;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
--- lisp/init-js2mode.el
+++ lisp/init-js2mode.el
@@ -2,4 +2,14 @@
       (append
        '(("\\.js\\'" . js2-mode))
        auto-mode-alist))
+
+(require 'nodejs-repl)
+;; mode 使用
+(setq auto-mode-alist
+      (append
+       '(("\\.html\\'" . web-mode)
+	 ("\\.css\\'" . web-mode)
+	 )
+       auto-mode-alist))
+(exec-path-from-shell-initialize)
 (provide 'init-js2mode)
