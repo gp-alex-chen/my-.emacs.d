69b69e8249e3062ccb7b7ec549f84bac64134f90
alex-chen <yuanzhang93.chen@gmail.com>
2016-07-31 00:29:46 +0800

    delete elpa

--- elpa/auto-complete-clang-20140409.52/auto-complete-clang-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; auto-complete-clang-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("auto-complete-clang.el") (22397 46704
-;;;;;;  32750 790000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; auto-complete-clang-autoloads.el ends here
