fd3a5b05d6602103078ea4f8f4cdd4534b2205f6
alex-chen <yuanzhang93.chen@gmail.com>
2016-08-17 21:17:56 +0800

    init-custom 中添加 上下屏调整为左右屏的方法

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -17,4 +17,23 @@ A|B => -
     ;; 选中原来 buffer 所在的 window
     (select-window (get-buffer-window buf))))
 
+(defun split-window-up/down->left/right ()
+  "把上下两屏调整为左右两屏.
+    A
+   --- A|B =>
+    B
+"
+  (interactive)
+  (let ((buf (current-buffer)))
+    ;; 选中上侧 window
+    (select-window (frame-first-window))
+    ;; 删掉右侧 window
+    (delete-other-windows)
+    ;; 分屏、选中右边 window
+    (select-window (split-window-right))
+    ;; 切换至最近 buffer
+    (switch-to-buffer (other-buffer))
+    ;; 选中原来 buffer 所在的 window
+    (select-window (get-buffer-window buf))))
+
 (provide 'init-custom)
